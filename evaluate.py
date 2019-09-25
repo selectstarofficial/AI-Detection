5f8b2a995acaaee33464b8eaa828a298146e2bbb
James Youngchae Chee <litcoderr@gmail.com>
2019-09-25 13:05:07 +0900

    adding total dataset functionality

--- evaluate.py
+++ evaluate.py
@@ -1,18 +1,38 @@
-from api import IntegratedApi
+from license_plate_api import LicensePlateDetector
+from face_detection_api import FaceDetector
+
+from license_plate_api.utils.parse_config import *
+from license_plate_api.utils.utils import *
+from license_plate_api.utils.datasets import *
 
 import argparse
+import os
 import torch
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 
-if __name__ == '__main__':
-    thresholds = []
-    for i in range(1, 10):
-        thresholds.append(i/10)
+def init_valid_text(path):
+    root = "license_plate_api/data/custom_with_face/labels"
+    list = os.listdir(root)
+    if not os.path.exists(path):
+        with open(path, "w") as file:
+            for name in list:
+                file.write("{}\n".format(os.path.join(root, name)))
 
-    for threshold in thresholds:
-        # TODO apply api when size mismatch problem is solved
-        # api = IntegratedApi(threshold=threshold)
-        # result = api.detect(some_image)
-        pass
+            file.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_config", type=str, default="total_dataset_config/", help="path to data config file")
+    parser.add_argument("--n_cpu", type=int, default=4, help="number of cpu threads to use during batch generation")
+    args = parser.parse_args()
+
+    # configure valid dataset
+    data_config = parse_data_config(os.path.join(args.data_config, "custom.data"))
+    valid_path = data_config["valid"]
+    class_names = load_classes(data_config["names"])
+    init_valid_text(data_config["valid"])
+
+    license_plate_model = LicensePlateDetector(mode="eval")
+    # face_model = FaceDetector()
 
--- license_plate_api/license_plate_detection.py
+++ license_plate_api/license_plate_detection.py
@@ -7,17 +7,18 @@ from .utils.datasets import *
 
 # TODO Make License Plate Detector -> Refer to detect.py in license_plate_api
 class LicensePlateDetector:
-    def __init__(self, threshold=0.5):
+    def __init__(self, threshold=0.5, mode="inference"):
         # Model Config
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         root = os.path.dirname(os.path.realpath(__file__))
-        self.weight_path = os.path.join(root, 'checkpoints', 'yolov3_ckpt_50.pth')
+        self.weight_path = os.path.join(root, 'yolov3_ckpt_98.pth')
         self.model_cfg = os.path.join(root, 'config', 'yolov3-custom.cfg')
         self.class_pth = os.path.join(root, 'config', 'classes.names')  # TODO add classes names file from cloud
         self.img_size = 416
         self.nms_thres = 0.5
         self.conf_thres = threshold
+        self.mode = mode
 
         # Load Model
         self.model = Darknet(self.model_cfg, img_size=self.img_size).to(self.device)
@@ -33,6 +34,7 @@ class LicensePlateDetector:
         self.classes = load_classes(self.class_pth)
 
     def detect(self, image):
+        mode = self.mode
         # 1. reformat image for input
         image_ = cv2.resize(image, (self.img_size, self.img_size))
         image_ = torch.Tensor(image_).permute(2,0,1).unsqueeze(0).to(self.device)
@@ -40,13 +42,18 @@ class LicensePlateDetector:
         # 2. inference
         result = self.model(image_)
         result = non_max_suppression(result, conf_thres=self.conf_thres, nms_thres=self.nms_thres)
-        result = result[0].cpu().numpy()
 
-        detections = []
-        # 3. resize to original image size
-        if result is not None:
-            detections = rescale_boxes(result, self.img_size, image.shape[:2])
-            detections = detections[detections[:, -1]==0]
-            detections = detections[:,:5]
+        if mode=="inference":
+            result = result[0].cpu().numpy()
+
+            detections = []
+            # 3. resize to original image size
+            if result is not None:
+                detections = rescale_boxes(result, self.img_size, image.shape[:2])
+                detections = detections[detections[:, -1]==0]
+                detections = detections[:,:5]
+        else:
+            detections = result
+
         return detections
 
