d376c570a9a3c13d53d8a89b1122425d70f05bac
Yoongi Kim <devlifecode@outlook.com>
2019-09-30 10:20:16 +0900

    settings.py typo fix

--- settings.py
+++ settings.py
@@ -10,9 +10,9 @@ class Settings:
     show_score = True  # True 이면 빨간색으로 클래스와 확신도를 표시합니다. False 이면 글자를 표시하지 않음.
     save_img = True  # True 이면 결과 이미지를 저장합니다. False 이면 이미지를 저장하지 않음.
     face_bbox_width_ratio = 0.6  # 얼굴 박스 너비 축소 비율입니다. (0.6이면 원래 크기의 60%로 줄입니다.)
-    face_bbox_height_ratio = 0.6  # 얼굴 박스 너비 축소 비율입니다. (0.6이면 원래 크기의 60%로 줄입니다.)
+    face_bbox_height_ratio = 0.6  # 얼굴 박스 높이 축소 비율입니다. (0.6이면 원래 크기의 60%로 줄입니다.)
     license_plate_bbox_width_ratio = 1.0  # 번호판 박스 너비 축소 비율.
-    license_plate_bbox_height_ratio = 1.0  # 번호판 박스 너비 축소 비율.
+    license_plate_bbox_height_ratio = 1.0  # 번호판 박스 높이 축소 비율.
     max_size_ratio = 0.33  # 크기가 너무 큰 물체는 필터링합니다. 0.33이면 전체 크기의 33%를 넘으면 무시합니다.
     license_plate_model_size = 704  # 모델이 학습된 크기입니다. 이 값은 바꾸지 마세요.
     xml_face_name = 'face'  # XML 파일에서 얼굴 태그의 라벨 이름을 지정합니다.
